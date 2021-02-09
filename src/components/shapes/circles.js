8a43a4f7eb3c95db38a0b8f348604c951a3126e7
Brandi Richardson <brandirichardson@Brandis-Air.attlocal.net>
2021-02-09 18:21:49 -0500

    circle

--- src/components/shapes/circles.js
+++ src/components/shapes/circles.js
@@ -1,38 +1,52 @@
 import React from 'react';
 
 const ACircle = () => {
-  const circleStyle = {
-    padding:10,
-    margin:20,
-    display:"inline-block",
-    backgroundColor: "#A40E4C",
-    borderRadius: "50%",
-    width:100,
-    height:100,
-  };
-  return (
-    <div style={circleStyle}>
-    </div>
-  );
+  
+}
 
-  var Circle = React.createClass({
+var Circle = React.createClass({
+
     render:function(){
+
       var circleStyle = {
         padding:10,
         margin:20,
         display:"inline-block",
-        backgroundColor: "#A40E4C",
+        backgroundColor: "#393E41", 
         borderRadius: "50%",
         width:100,
         height:100,
       };
+
       return (
         <div style={circleStyle}>
         </div>
       );
     }
   });
-}
+
+
+  var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363",
+                   "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
+  
+  var renderData = [];
+  
+  for (var i = 0; i < colors.length; i++) {
+    var color = colors[i];
+    renderData.push(<Circle key={i + color} bgColor={color}/>);
+  }
+
+
+  var destination = document.querySelector("#container");
+  
+//   ReactDOM.render(
+//     <div>
+//       {renderData}
+//     </div>,
+//     destination
+//   );
+
+
 
 const WCircle = () => {
     return (
