55f3ae8f0e92eb69168eabb3f6d0ccf7aeef1de9
evperlman <evperlman@gmail.com>
2021-02-09 10:19:16 -0800

    updated main

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div>
       Two Tone
+      <div>
+      <audio
+        controls
+        src="/media/cc0-audio/t-rex-roar.mp3">
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio>
+      </div>
     </div>
   )
 };
