578f7ee57048176e353d3fd29fe37ca7ea8c220a
Giao Tran <58094472+gd-tran@users.noreply.github.com>
2021-02-09 17:00:40 -0800

    created Recorder component, added recording functionality
    Co-authored-by: Giao Tran <contactgiaotran@gmail.com>
    Co-authored-by: Evan Perlman <evperlman@gmail.com>

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,12 +1,86 @@
 import React from "react";
 
 const Recorder = () => {
+  let clicked1 = false;
+  let clicked2 = false
+  const chunks = [];
+  const ac = new AudioContext()
+  const osc1 = ac.createOscillator();
+  const osc2 = ac.createOscillator();
+  osc2.type = 'sine'
+  const dest = ac.createMediaStreamDestination();
+  const mediaRecorder = new MediaRecorder(dest.stream);
+  osc1.connect(dest);
+  osc2.connect(dest);
+
+  mediaRecorder.ondataavailable = (e) => {
+    // push each chunk (blobs) in an array
+    chunks.push(e.data);
+  };
+
+  mediaRecorder.onstop = (e) => {
+    // Make blob out of our blobs, and open it.
+    const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+    document.getElementById("test").src = URL.createObjectURL(blob);
+  };
+
+
+// add a second a button
+// two buttons, each has a corresponding oscilator. 
+// on click, the oscilators connect and broadcast a tone locally. 
+// both oscilators stream to the same destination. 
+// 
+
+  const record = (e) => {
+    console.log('sound1')
+    if (!clicked1){
+      mediaRecorder.start();
+      osc1.start(0);
+      e.target.textContent = "Stop recording";
+      clicked1 = true;
+    } 
+    else {
+      // mediaRecorder.stop();
+      osc1.stop(0);
+      e.target.disabled = true;
+    }
+  }
+
+  const record2 = (e) => {
+    console.log('sound2')
+    if (!clicked2){
+      // mediaRecorder.start();
+      osc2.frequency.value = 660;
+      osc2.start(0);
+      e.target.textContent = "Stop recording";
+      clicked2 = true;
+    } 
+    else {
+      // mediaRecorder.stop();
+      osc2.stop(0);
+      e.target.disabled = true;
+    }
+  }
+
+  const stopRecording = (e) => {
+    console.log('stopped the recording')
+
+    mediaRecorder.stop()
+    osc1.stop(0)
+    osc2.stop(0)
+  }
 
 return (
   <div>
     <h1>Recorder Element</h1>
+    <p>Encoding a pure sine wave to an Opus file </p>
+    <button onClick={record}>Sound 1</button>
+    <button onClick={record2}>Sound 2</button>
+    <button onClick={stopRecording}>STOP RECORDING</button>
+    <audio id="test" controls></audio>
+    {/* {audioPlayer} */}
   </div>
   )
-}
+};
 
 export { Recorder }
