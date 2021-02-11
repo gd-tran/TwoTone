9ce72dc2bfa35ea1a5ecac4e803039cbb56f99aa
evperlman <evperlman@gmail.com>
2021-02-11 12:19:31 -0800

    updated filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import React, { Component, useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Visualizer from './components/Visualizer';
 import { Recorder } from './components/Recorder';
-import { Settings } from './components/Settings'
-import { Control } from './components/Control'
+import { Settings } from './components/Settings';
+import { Control } from './components/Control';
+import NewSound from './components/Filter';
 import './styles.scss'
 const App = () => {
 
   return (
     <div>
       {/* <Switch>
-        <Route exact path='/login'>
-          <Landing />
-        </Route>
-        <Route exact path='/visualizer'> */}
+        <Route exact path='/visualizer'>
           <Visualizer />
-          {/* <Settings /> */}
-          {/* <Recorder /> */}
-          {/* <Control /> */}
-        {/* </Route>
-      </Switch> */}
+        </Route>
+        <Route exact path='/sound2'>
+          <NewSound />
+          <Settings /> 
+          <Recorder />
+          <Control />
+        </Route>
+      </Switch>  */}
+      <Filter />
     </div>
   )
 };
